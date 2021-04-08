2156e6dcf0bb3dd2ca96332b83a30991126a1b17
Shashank Mahesh <smahesh3@illinois.edu>
2021-04-08 12:48:37 -0700

    made setup to bash script

--- main.py
+++ main.py
@@ -2,15 +2,3 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-
-
-#source .venv/bin/activate
-#export FLASK_APP=app
-#export FLASK_DEBUG=1
-#Show Player that is there
-#Show Player that is not there
-#Add Player 
-#Search for Player
-#Update Player
-#Remove Player›
